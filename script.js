0a32a4e137c194c0076af67c3da481c31ccd18af
dheeraz15 <dhirajchapagain353@gmail.com>
2025-12-16 22:43:47 +0545

    yes

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function closeModal(modalId) {
 }
 
 // Close modal when clicking outside
-window.onclick = function(event) {
+window.onclick = function (event) {
     const videoModal = document.getElementById('videoModal');
     const infoModal = document.getElementById('infoModal');
     if (event.target == videoModal) {
@@ -19,5 +19,34 @@ window.onclick = function(event) {
     }
     if (event.target == infoModal) {
         closeModal('infoModal');
+    }
+}
+
+function toggleMute() {
+    const audio = document.getElementById('hero-music');
+    const btnHtml = document.querySelector('.mute-btn');
+
+    if (audio.paused) {
+        audio.play().then(() => {
+            // Change icon to Volume Up
+            btnHtml.innerHTML = `
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M11 5L6 9H2V15H6L11 19V5Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M19.07 4.93C20.9447 6.80527 21.9979 9.34836 21.9979 12C21.9979 14.6516 20.9447 17.1947 19.07 19.07M15.54 8.46C16.4774 9.39763 17.0039 10.6692 17.0039 12C17.0039 13.3308 16.4774 14.6024 15.54 15.54" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+            `;
+        }).catch(err => {
+            console.error("Audio play failed (user interaction needed):", err);
+        });
+    } else {
+        audio.pause();
+        // Change icon to Mute
+        btnHtml.innerHTML = `
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M11 5L6 9H2V15H6L11 19V5Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <line x1="23" y1="9" x2="17" y2="15" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <line x1="17" y1="9" x2="23" y2="15" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+        `;
     }
 }
